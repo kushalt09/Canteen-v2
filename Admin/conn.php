17b62ab2e3831d2fd88f1912b717f89733baa98b
कुशल किरण  तिमिल्सिना <101187569+kushalt09@users.noreply.github.com>
2024-02-29 10:58:51 +0545

    Update conn.php

--- Admin/conn.php
+++ Admin/conn.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "root";
 $dbname = "canteen";
 $conn = new mysqli($servername, $username, $password,$dbname);
 ?>
